e8f8c9ab5db75c47e23f2e86e9eb55e55798fdff
Luiz Henrique <104238381+LuizCocada@users.noreply.github.com>
2023-09-11 13:29:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+/*  1
+alert("Hello word")
+*/
+
+
+/*  2
+let number1 = 3
+let number2 = 5
+
+sum = number1 + number2 
+
+alert(sum)
+*/
+
+/*  3
+const valor = prompt("Verifique se o seu valor é um número")
+
+if(isNaN(valor)){
+    alert(`O valor NAO é um numero. ${valor}`)
+}
+else{ 
+    alert(`O valor é um numero. ${valor}`)
+}
+*/
+
+/*  4
+const valor = prompt(`Verifique se o valor é uma string`)
+
+if(isNaN(valor)){
+    alert(`O valor é uma String`)
+}
+else{
+    alert(`O valor é um Number`)
+}
+*/
+
+
+/*  5
+const a = true
+if(typeof a === `boolean`){
+    alert(`O valor é Boolean`)
+} else{
+    alert(`O valor nao é do tipo Boolean`)
+}
+*/
+
+
+/*  6
+const number1 = Number(prompt(`Digite o Primeiro numero:`))
+const number2 = Number(prompt(`Digite o Segundo numero:`))
+
+let result = number1 - number2
+
+alert(result)
+*/
+
+/*  7
+const number1 = Number(prompt(`Digite o Primeiro numero:`))
+const number2 = Number(prompt(`Digite o Segundo numero:`))
+
+let result = number1 * number2
+
+alert(result)
+*/
+
+/*  8
+const number1 = Number(prompt(`Digite o Primeiro numero:`))
+const number2 = Number(prompt(`Digite o Segundo numero:`))
+
+let result = number1 / number2
+
+alert(result)
+*/
+
+/*  9
+let number = Number(prompt(`Digite um numero PAR:`))
+
+if(number %2 == 0){
+    alert(`Esse numero é PAR`)
+} else{
+    alert(`NAO é um numero PAR`)
+}
+*/
+
+/*  10
+let number = Number(prompt(`Digite um numero IMPAR:`))
+
+if(number %2 == 1){
+    alert(`Esse numero é IMPAR`)
+} else{
+    alert(`Esse numero NAO é IMPAR`)
+}
+*/
